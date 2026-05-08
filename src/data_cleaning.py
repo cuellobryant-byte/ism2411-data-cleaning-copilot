2601286f6a6c2cae791e9aedbf6deac12c892eb0
cuellobryant-byte <cuellobryant@gmail.com>
2026-05-08 14:07:11 -0400

    Create data_cleaning.py
    Add function to load raw CSV data

---
+++ src/data_cleaning.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def load_data(file_path: str):
+
+    try:
+        df = pd.read_csv(file_path)
+        print("Raw dataset loaded successfully.")
+        return df
+
+    except FileNotFoundError:
+        print(f"Error: File not found at {file_path}")
+        return None
